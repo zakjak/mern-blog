bd8e694261848b7da8bb7750c6c230ca6367b184
zakjak <78931393+zakjak@users.noreply.github.com>
2024-01-23 02:54:10 -0600

    Create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ function Header() {
                 <FaMoon />
             </Button>
             <Link>
-                <Button gradientDuoTone='purpleToBlue' >
+                <Button gradientDuoTone='purpleToBlue' outline>
                     Sign In
                 </Button>
             </Link>
