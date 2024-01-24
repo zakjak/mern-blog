ae05099edaf082c9b984087d352daab158444a00
zakjak <78931393+zakjak@users.noreply.github.com>
2024-01-24 03:56:09 -0600

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.module.js'
 import bcrypt from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
@@ -27,5 +28,41 @@ export const signup = async (req, res, next) => {
     }catch(err){
         next(err)
     }
+}
 
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body
+
+    if(!email || 
+        !password || 
+        email === '' || 
+        password === ''){
+            next(errorHandler(400, 'All fields are required'))
+        }
+
+    try{
+        const validUser = await User.findOne({ email })
+        if(!validUser){
+            next(errorHandler(404, 'User not found'))
+        }
+
+        const validPassword = bcrypt.compareSync(password, validUser.password)
+
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid password'))
+        }
+
+        const token = jwt.sign(
+            {id: validUser._id},
+            process.env.JWT_SECRET
+        )
+
+        const {password: pass, ...rest} = validUser._doc
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true}).json(rest)
+    }catch(err){
+        next(err)
+    }
 }
