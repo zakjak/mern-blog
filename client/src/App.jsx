6711d1be2cc536fb8d564c2b56daf1c41f0ece6b
zakjak <78931393+zakjak@users.noreply.github.com>
2024-01-24 18:14:14 -0600

    update header component with user data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
       <FooterComponent />
