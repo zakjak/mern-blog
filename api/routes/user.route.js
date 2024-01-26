b903bf452c2017f80464e6bada9940c98cc68673
zakjak <78931393+zakjak@users.noreply.github.com>
2024-01-26 01:51:12 -0600

    add update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { signup } from '../controllers/auth.controller.js'
+import { updateUser } from '../controllers/user.controller.js'
+import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
 
 router.post('/signup', signup)
+router.put('/update/:userId', verifyToken,updateUser)
 
 export default router
