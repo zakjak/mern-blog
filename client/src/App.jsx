64d919f8f76530a316f2c66378ae379593846a52
zakjak <78931393+zakjak@users.noreply.github.com>
2024-01-28 22:52:33 -0600

    add comment section to the post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
 import PostPage from './pages/PostPage'
+import ScrollToTop from './components/ScrollToTop'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
 
   return (
     <BrowserRouter>
+    <ScrollToTop />
     <Header />
       <Routes>
         <Route path='/' element={<Home />} />
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Button, Spinner } from 'flowbite-react'
 import CallToAction from '../components/CallToAction'
+import CommentSection from '../components/CommentSection'
 
 function PostPage() {
     const { postSlug } = useParams()
@@ -57,6 +58,7 @@ function PostPage() {
         <div className="maxs-w-4xl mx-auto w-fulls">
             <CallToAction />
         </div>
+        <CommentSection postId={post._id} />
     </main>
   )
 }
